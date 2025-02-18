d5876769a643c2d9bc36fc7540a688e9c3e43ce8
Carlos Jefté <134069354+carlosjefte@users.noreply.github.com>
2025-02-17 21:19:48 -0300

    fix: frist employee not being detected
    feat: frequency distribution code

--- frequencia.c
+++ frequencia.c
@@ -2,9 +2,46 @@
 #include "frequencia.h"
 
 void calcularDistribuicaoFrequencia(ListaFuncionarios *lista) {
-    printf("Cálculo da distribuição de frequência ainda não implementado.\n");
+    float soma = 0.0, mult = 0.0;
+    float limite_inferior[] = {0.0, 1518.0, 3036.0, 7590.0, 9108.0};
+    float pontosMedios[] = {759, 2277, 5313, 8349, 9867};
+    float limite_superior[] = {1518, 3036, 7590, 9108, 10626};
+    
+    int frequencia [] = {0,0,0,0,0};
+    
+    // Calcula a frequência de cada intervalo
+    for(int i = 0; i < lista->quantidade; i++){ 
+        for(int j = 0; j < 5; j++){
+            if(lista->funcionarios[i].salario > limite_inferior[j] && lista->funcionarios[i].salario <= limite_superior[j]){
+                frequencia[j]++;
+                break; // Para evitar contagem em múltiplos intervalos
+            }
+        }
+    }
+
+    // Imprime a distribuição de frequência
+    printf("------------------------------------- \n");
+    printf("I.C*                               fi*\n");
+    printf("------------------------------------- \n");
+    for (int k = 0; k < 5; k++) {
+        printf("Faixa %.2f - %.2f: %d funcionários\n", 
+                limite_inferior[k], 
+                limite_superior[k], 
+                frequencia[k]);
+    }
+    
+    // Calcula a média intervalar
+    for(int i = 0 ; i < 5 ; i++){
+        soma += frequencia[i];
+        mult += frequencia[i] * pontosMedios[i];
+    }
+    
+    float media = mult / soma;
+    printf("A média intervalar é: %.2f \n", media);
 }
 
+
 float calcularSalarioModal(ListaFuncionarios *lista) { // Corrigido: agora recebe ListaFuncionarios *
-    return lista->quantidade > 0 ? lista->funcionarios[0].salario : 0;
+    return printf("ok");
+    // return lista->quantidade > 0 ? lista->funcionarios[0].salario : 0;
 }
